e128b83acef6d5f00ad00a3e7d7a32b602a6d93c
manmaed <manmaed18@gmail.com>
2015-05-16 11:30:41 +0100

    Revert "undo me"
    This reverts commit bb0081d2a975f00dfefbaa219760d775d910fd33.

--- src/main/java/com/YunusKayne/PetRock/init/Blocks.java
+++ src/main/java/com/YunusKayne/PetRock/init/Blocks.java
@@ -1,9 +1,12 @@
 package com.YunusKayne.PetRock.init;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
 import com.YunusKayne.PetRock.blocks.blockPetrium;
 import com.YunusKayne.PetRock.blocks.orePetrium;
+
 import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.block.material.Material;
 
 public class Blocks
 {
--- src/main/java/com/YunusKayne/PetRock/init/Recipes.java
+++ src/main/java/com/YunusKayne/PetRock/init/Recipes.java
@@ -1,5 +1,7 @@
 package com.YunusKayne.PetRock.init;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.registry.GameRegistry;
 
