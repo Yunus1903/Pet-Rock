0eb344753a86d4cbedc224eb5da3700a932fceeb
Yunus1903 <yunus1903dj@gmail.com>
2015-06-26 12:50:01 +0200

    Update after Fix

--- src/main/java/com/YunusKayne/PetRock/client/handler/KeyInputEventHandler.java
+++ src/main/java/com/YunusKayne/PetRock/client/handler/KeyInputEventHandler.java
@@ -1,23 +1,21 @@
 package com.YunusKayne.PetRock.client.handler;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-
-import com.YunusKayne.PetRock.client.settings.Keybindings;
+import com.YunusKayne.PetRock.client.settings.KeyBindings;
 import com.YunusKayne.PetRock.reference.Key;
 import com.YunusKayne.PetRock.utility.ChatHelper;
 import com.YunusKayne.PetRock.utility.LogHelper;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.InputEvent;
+import net.minecraft.entity.player.EntityPlayer;
 
 public class KeyInputEventHandler
 {	
 	private static Key getPressedKeyBinding(EntityPlayer player)
 	{
-		if(Keybindings.Hello.isPressed())
+		if(KeyBindings.Hello.isPressed())
 		{
-			ChatHelper.ChatMessage("Hi " + player.getCommandSenderName() + "!", player);
+			ChatHelper.ChatMessageString("Hi " + player.getCommandSenderName() + "!");
 			LogHelper.info("Working!");
 			return Key.Hello;
 		}
