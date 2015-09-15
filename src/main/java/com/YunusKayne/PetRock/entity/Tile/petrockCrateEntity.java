557d8dc57933cf430cc9bf3161d05c92afaf93de
Yunus1903 <yunus1903dj@gmail.com>
2015-09-15 22:39:21 +0200

    some stuff

--- src/main/java/com/YunusKayne/PetRock/entity/Tile/petrockCrateEntity.java
+++ src/main/java/com/YunusKayne/PetRock/entity/Tile/petrockCrateEntity.java
@@ -1,5 +1,6 @@
 package com.YunusKayne.PetRock.entity.Tile;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 
 public class petrockCrateEntity extends TileEntity
